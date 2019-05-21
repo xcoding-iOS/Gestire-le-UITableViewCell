ef3159f88833fa3b4769daf82d8cc45e427531bb
Giuseppe Sapienza <sapienza.giuseppe93@gmail.com>
2019-05-21 14:03:24 +0200

    completed project

--- Managing UITableViewCell/View/MatchTableViewCell.swift
+++ Managing UITableViewCell/View/MatchTableViewCell.swift
@@ -25,7 +25,47 @@ class MatchTableViewCell: UITableViewCell {
     
     @IBOutlet weak var stadiumLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
-
+    
+    var match: Match? {
+        didSet {
+            self.homeNameLabel.text = match?.home.name
+            self.homeImageView.image = match?.home.image
+            
+            self.outNameLabel.text = match?.out.name
+            self.outImageView.image = match?.out.image
+            
+            self.dateLabel.text = match?.date
+            self.stadiumLabel.text = match?.stadium
+            
+            if let live = match?.live {
+                self.liveButton.isHidden = false
+                self.homeResultStack.isHidden = false
+                self.outResultStack.isHidden = false
+                self.homeGoalLabel.text = "\(live.homeGoal)"
+                self.outGoalLabel.text = "\(live.outGoal)"
+                
+                if (live.homeGoal > live.outGoal) {
+                    self.homeGoalLabel.textColor = #colorLiteral(red: 0.9267585874, green: 0.367726624, blue: 0.3804723024, alpha: 1)
+                    self.outGoalLabel.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+                } else if (live.homeGoal < live.outGoal) {
+                    self.homeGoalLabel.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+                    self.outGoalLabel.textColor = #colorLiteral(red: 0.9267585874, green: 0.367726624, blue: 0.3804723024, alpha: 1)
+                }
+                
+            } else {
+                self.liveButton.isHidden = true
+                self.homeResultStack.isHidden = true
+                self.outResultStack.isHidden = true
+            }
+        }
+    }
+    
+    @IBAction func shareButton_clicked(_ sender: UIButton) {
+        print("Share...")
+        print("Home: \(self.match!.home) - Out: \(self.match!.out)")
+        /* Il codice per lo share */
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
--- Managing UITableViewCell/ViewController.swift
+++ Managing UITableViewCell/ViewController.swift
@@ -54,35 +54,7 @@ extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let match = self.matchs[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "matchCell", for: indexPath) as! MatchTableViewCell
-        cell.homeNameLabel.text = match.home.name
-        cell.homeImageView.image = match.home.image
-        
-        cell.outNameLabel.text = match.out.name
-        cell.outImageView.image = match.out.image
-        
-        cell.dateLabel.text = match.date
-        cell.stadiumLabel.text = match.stadium
-        
-        if let live = match.live {
-            cell.liveButton.isHidden = false
-            cell.homeResultStack.isHidden = false
-            cell.outResultStack.isHidden = false
-            cell.homeGoalLabel.text = "\(live.homeGoal)"
-            cell.outGoalLabel.text = "\(live.outGoal)"
-            
-            if (live.homeGoal > live.outGoal) {
-                cell.homeGoalLabel.textColor = #colorLiteral(red: 0.9267585874, green: 0.367726624, blue: 0.3804723024, alpha: 1)
-                cell.outGoalLabel.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
-            } else if (live.homeGoal < live.outGoal) {
-                cell.homeGoalLabel.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
-                cell.outGoalLabel.textColor = #colorLiteral(red: 0.9267585874, green: 0.367726624, blue: 0.3804723024, alpha: 1)
-            }
-            
-        } else {
-            cell.liveButton.isHidden = true
-            cell.homeResultStack.isHidden = true
-            cell.outResultStack.isHidden = true
-        }
+        cell.match = match
         return cell
     }
 }
